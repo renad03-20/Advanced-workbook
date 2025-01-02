3866ec92166f8f3c8eb8bb23fe71b1ce4056f048
Rinad adel <studentrenad@gmail.com>
2025-01-02 09:11:55 +0300

    added a function to find duplicate and extra XML files

--- task5/recursive.py
+++ task5/recursive.py
@@ -2,10 +2,7 @@ import xml.etree.ElementTree as ET # import the xml module
 import sys # import the sys module 
 sys.setrecursionlimit(2000) # set the recursion limit to 2000 
 
-# xmlfile = '/Users/renadcute/Desktop/Advanced workbook/task5/empty.xml' # path to the xml file #!this is an ampty file
-# xmlfile = '/Users/renadcute/Desktop/Advanced workbook/task5/malformed.xml' # path to the xml file, this is a malformed file
-# xmlfile = '/Users/renadcute/Desktop/Advanced workbook/task5/sample2.xml' # path to the xml file, this is a valid file
-xmlfile = '/Users/renadcute/Desktop/Advanced workbook/task5/sample.xml' # path to the xml file, this is a broken file
+xmlfile = 'task5/ proper_file.xml' # path to the xml file, This is a proper file
 
 try:
     tree = ET.parse(xmlfile) # parse the xml file 
@@ -13,6 +10,20 @@ try:
 except (ET.ParseError):
     print(' The file is empty or invalid')
     exit()
+
+# Dysfunction finds duplicate elements in the XML file
+seen_elements = set()
+def find_duplicate(value, attribute_name=None, track_specific=None):
+    global seen_elements
+    # if the attribute name is not in the track_specific list, return
+    if track_specific is not None and attribute_name not in track_specific:
+        return
+    # if the value is in the seen_elements set, print the duplicate value
+    if value in seen_elements:
+        print(f"Duplicate value detected: {value}")
+    # add the value to the seen_element list
+    else:
+        seen_elements.add(value)
 
 # a function to find broken elements in the xml file 
 broken_elements = []
@@ -24,6 +35,8 @@ def find_broken_element(element, path=''):
         # defining what broken element is 
         if attribute_value in ('null', 'None', '', 'unknown'):
             broken_elements.append(f'Broken element: {attribute_name}="{attribute_value}" at {current_path}')
+        
+        find_duplicate(attribute_value, attribute_name, track_specific={'id'}) # Specify what you don't want any duplicates of 
 
     #iterating throuhg the children of the element      
     for child in element:
